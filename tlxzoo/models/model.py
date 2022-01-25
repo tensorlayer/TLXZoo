46aa12aa133406d72f38f2a8a422452342cfc5a6
xiaolong.xu <453935413@qq.com>
2022-01-25 10:56:11 +0800

    Add ModuleFromPretrainedMixin

--- tlxzoo/models/model.py
+++ tlxzoo/models/model.py
@@ -2,10 +2,10 @@ from tensorlayerx import nn, logging
 import os
 from ..utils import MODEL_WEIGHT_FORMAT, MODEL_WEIGHT_NAME
 from ..config.config import BaseModelConfig
-from tensorlayerx.files import maybe_download_and_extract
+from ..utils.from_pretrained import ModuleFromPretrainedMixin
 
 
-class BaseModule(nn.Module):
+class BaseModule(nn.Module, ModuleFromPretrainedMixin):
     config_class = BaseModelConfig
 
     def __init__(self, config, *args, **kwargs):
@@ -14,43 +14,5 @@ class BaseModule(nn.Module):
         super(BaseModule, self).__init__(*args, **kwargs)
         self.config = config
 
-    @classmethod
-    def from_pretrained(cls, pretrained_base_path, *model_args, **kwargs):
-        config = kwargs.pop("config", None)
-
-        if config is None:
-            if pretrained_base_path is None:
-                raise ValueError("pretrained_base_path and config are both None.")
-            config = cls.config_class.from_pretrained(pretrained_base_path, **kwargs)
-
-        model = cls(config, *model_args, **kwargs)
-
-        weights_path = config.weights_path
-        if not weights_path.startswith("http"):
-            if pretrained_base_path is None:
-                logging.warning("Don't load weight.")
-                return model
-
-            weights_path = os.path.join(pretrained_base_path, weights_path)
-        else:
-            if pretrained_base_path is None:
-                pretrained_base_path = ".cache"
-            url, name = weights_path.rsplit("/", 1)
-            maybe_download_and_extract(name, pretrained_base_path, url + "/")
-            weights_path = os.path.join(pretrained_base_path, name)
-
-        model.load_weights(weights_path)
-        return model
-
     def save_pretrained(self, save_directory):
-        if os.path.isfile(save_directory):
-            logging.error(f"Save directory ({save_directory}) should be a directory, not a file")
-            return
-
-        os.makedirs(save_directory, exist_ok=True)
-
-        # save weight
-        self.save_weights(os.path.join(save_directory, MODEL_WEIGHT_NAME), MODEL_WEIGHT_FORMAT)
-
-        # save config
-        self.config.save_pretrained(save_directory)
+        self._save_pretrained(save_directory, MODEL_WEIGHT_NAME, MODEL_WEIGHT_FORMAT)
--- tlxzoo/task/task.py
+++ tlxzoo/task/task.py
@@ -2,10 +2,10 @@ from tensorlayerx import nn, logging
 import os
 from ..utils import TASK_WEIGHT_FORMAT, TASK_WEIGHT_NAME
 from ..config.config import BaseTaskConfig
-from tensorlayerx.files import maybe_download_and_extract
+from ..utils.from_pretrained import ModuleFromPretrainedMixin
 
 
-class BaseTask(nn.Module):
+class BaseTask(nn.Module, ModuleFromPretrainedMixin):
     config_class = BaseTaskConfig
 
     def __init__(self, config: BaseTaskConfig, *args, **kwargs):
@@ -13,46 +13,8 @@ class BaseTask(nn.Module):
         super(BaseTask, self).__init__(*args, **kwargs)
         self.config = config
 
-    @classmethod
-    def from_pretrained(cls, pretrained_base_path, *task_args, **kwargs):
-        config = kwargs.pop("config", None)
-
-        if config is None:
-            if pretrained_base_path is None:
-                raise ValueError("pretrained_base_path and config are both None.")
-            config = cls.config_class.from_pretrained(pretrained_base_path, **kwargs)
-
-        task = cls(config, *task_args, **kwargs)
-
-        weights_path = config.weights_path
-
-        if not weights_path.startswith("http"):
-            if pretrained_base_path is None:
-                logging.warning("Don't load weight.")
-                return task
-
-            weights_path = os.path.join(pretrained_base_path, weights_path)
-        else:
-            if pretrained_base_path is None:
-                pretrained_base_path = ".cache"
-            url, name = weights_path.rsplit("/", 1)
-            maybe_download_and_extract(name, pretrained_base_path, url + "/")
-            weights_path = os.path.join(pretrained_base_path, name)
-        task.load_weights(weights_path)
-        return task
-
     def save_pretrained(self, save_directory):
-        if os.path.isfile(save_directory):
-            logging.error(f"Save directory ({save_directory}) should be a directory, not a file")
-            return
-
-        os.makedirs(save_directory, exist_ok=True)
-
-        # save weight
-        self.save_weights(os.path.join(save_directory, TASK_WEIGHT_NAME), TASK_WEIGHT_FORMAT)
-
-        # save config
-        self.config.save_pretrained(save_directory)
+        self._save_pretrained(save_directory, TASK_WEIGHT_NAME, TASK_WEIGHT_FORMAT)
 
 
 class BaseForImageClassification(BaseTask):
