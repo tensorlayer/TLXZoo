563f6b57d32a245f8e22f6b922482449d1f77492
jianzhnie <jianzhnie@126.com>
2022-01-27 17:46:22 +0800

    Update alexnet.py

--- tlxzoo/models/alexnet/alexnet.py
+++ tlxzoo/models/alexnet/alexnet.py
@@ -6,7 +6,7 @@ import numpy as np
 import tensorlayerx as tlx
 from tensorlayerx import logging
 from tensorlayerx.nn import (BatchNorm, Conv2d, Dense, Flatten, ReLU, Dropout,
-                             SequentialLayer, MaxPool2d, AdaptiveAvgPool2d)
+                             SequentialLayer, MaxPool2d, AdaptiveMeanPool2d)
 from ...utils.output import BaseModelOutput
 from dataclasses import dataclass
 from .config_alexnet import AlexNetModelConfig
@@ -27,31 +27,49 @@ class AlexNet(BaseModule):
         dropout = config.dropout
 
         layer_list = []
-        layer_list.append(Conv2d(in_channels=3, n_filter= 64, filter_size=(11,11) stride=4, padding=2))
+        layer_list.append(
+            Conv2d(in_channels=3,
+                   n_filter=64,
+                   filter_size=(11, 11),
+                   strides=(4, 4),
+                   padding=2))
         layer_list.append(ReLU(inplace=True))
-        layer_list.append(MaxPool2d(filter_size=3, stride=2))
-        layer_list.append(Conv2d(64, 192, filter_size=5, padding=2))
+        layer_list.append(MaxPool2d(filter_size=(3, 3), stride=(2, 2)))
+        layer_list.append(
+            Conv2d(in_channels=64, n_filter=192, filter_size=(5, 5),
+                   padding=2))
         layer_list.append(ReLU(inplace=True))
-        layer_list.append(MaxPool2d(filter_size=3, stride=2))
-        layer_list.append(Conv2d(192, 384, filter_size=3, padding=1))
+        layer_list.append(MaxPool2d(filter_size=(3, 3), stride=(2, 2)))
+        layer_list.append(
+            Conv2d(in_channels=192,
+                   n_filter=384,
+                   filter_size=(3, 3),
+                   padding=1))
         layer_list.append(ReLU(inplace=True))
-        layer_list.append(Conv2d(384, 256, filter_size=3, padding=1))
+        layer_list.append(
+            Conv2d(in_channels=384,
+                   n_filter=256,
+                   filter_size=(3, 3),
+                   padding=1))
         layer_list.append(ReLU(inplace=True))
-        layer_list.append(Conv2d(256, 256, filter_size=3, padding=1))
+        layer_list.append(
+            Conv2d(in_channels=256,
+                   n_filter=256,
+                   filter_size=(3, 3),
+                   padding=1))
         layer_list.append(ReLU(inplace=True))
-        layer_list.append(MaxPool2d(filter_size=3, stride=2))
-
+        layer_list.append(MaxPool2d(filter_size=(3, 3), stride=(2, 2)))
         self.features = SequentialLayer(layer_list)
-        self.avgpool = AdaptiveAvgPool2d((6, 6))
+        self.avgpool = AdaptiveMeanPool2d((6, 6))
 
         output_list = []
         output_list.append(Dropout(p=dropout))
-        output_list.append(Dense(256 * 6 * 6, 4096))
+        output_list.append(Dense(inchannles=256 * 6 * 6, n_units=4096))
         output_list.append(ReLU(inplace=True))
         output_list.append(Dropout(p=dropout))
-        output_list.append(Dense(4096, 4096))
+        output_list.append(Dense(inchannles=4096, n_units=4096))
         output_list.append(ReLU(inplace=True))
-        output_list.append(Dense(4096, num_classes))
+        output_list.append(Dense(inchannles=4096, n_units=num_classes))
 
         self.classifier = SequentialLayer(layer_list)
 
